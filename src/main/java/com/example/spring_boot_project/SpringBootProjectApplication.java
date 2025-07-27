d24f01c9c92550e4dd676e1b7443f1cadea159b7
GabrielNat1 <dcbiel846@gmail.com>
2025-07-27 19:12:05 -0300

    feat: set up dotenv to manage environment variables and add env.example

--- src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
+++ src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
@@ -1,12 +1,13 @@
 package com.example.spring_boot_project;
 
+import config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
 	public static void main(String[] args) {
+		DotenvInitializer.init();
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
-
 }
