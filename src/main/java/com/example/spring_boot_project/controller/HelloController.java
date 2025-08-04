1e70b1330a01a5f6edfd97be13f89130f43b1d36
GabrielNat1 <dcbiel846@gmail.com>
2025-08-03 21:18:16 -0300

    fix: fixed hellocontroller.java

--- src/main/java/com/example/spring_boot_project/controller/HelloController.java
+++ src/main/java/com/example/spring_boot_project/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_project;
+package com.example.spring_boot_project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
