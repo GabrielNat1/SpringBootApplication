4a663ce3c9e7b235b53a01bfb93e9a90141afc2c
GabrielNat1 <dcbiel846@gmail.com>
2025-11-01 11:28:13 -0300

    refactor(config): clean up main class and integrate DotenvInitializer properly

--- src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
+++ src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
@@ -1,35 +1,15 @@
 package com.example.spring_boot_project;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.spring_boot_project.config.DotenvInitializer;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        setSystemPropertyIfPresent(dotenv, "ADMIN_USERNAME");
-        setSystemPropertyIfPresent(dotenv, "ADMIN_PASSWORD");
-        setSystemPropertyIfPresent(dotenv, "JWT_SECRET_KEY");
-        setSystemPropertyIfPresent(dotenv, "SERVER_COMPRESSION_MIN_RESPONSE_SIZE");
-
-        SpringApplication.run(SpringBootProjectApplication.class, args);
-    }
-
-    /**
-     * verify system property if present
-     *
-     * @param dotenv
-     * @param key
-     * @return void
-     */
-    private static void setSystemPropertyIfPresent(Dotenv dotenv, String key) {
-        String value = dotenv.get(key);
-        if (value != null) {
-            System.setProperty(key, value);
-        }
+    	SpringApplication app = new SpringApplication(SpringBootProjectApplication.class);
+        app.addInitializers(new DotenvInitializer());
+        app.run(args);
     }
 }
