29ee6f6035bf24fd645e221c04dc90ddb77ea624
GabrielNat1 <dcbiel846@gmail.com>
2025-11-01 18:21:00 -0300

    feat: implements caching

--- src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
+++ src/main/java/com/example/spring_boot_project/SpringBootProjectApplication.java
@@ -2,10 +2,12 @@ package com.example.spring_boot_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import com.example.spring_boot_project.config.DotenvInitializer;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootProjectApplication {
     public static void main(String[] args) {
     	SpringApplication app = new SpringApplication(SpringBootProjectApplication.class);
--- src/main/java/com/example/spring_boot_project/config/CacheConfig.java
+++ src/main/java/com/example/spring_boot_project/config/CacheConfig.java
@@ -1,21 +1,30 @@
 package com.example.spring_boot_project.config;
 
 import com.example.spring_boot_project.serializer.RedisCacheGZIPSerializer;
+
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class CacheConfig {
-    @Bean
-    @Primary
-    public RedisCacheConfiguration defaultCacheConfig(){
-        RedisCacheGZIPSerializer serializerGzip = new RedisCacheGZIPSerializer();
+	@Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheGZIPSerializer gzipSerializer = new RedisCacheGZIPSerializer();
 
-        return RedisCacheConfiguration
-                .defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializerGzip));
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)) // TTL 30 minutes
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(gzipSerializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
