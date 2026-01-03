c3dca820e15188a70c9935f9978517c7576c3898
GabrielNat1 <dcbiel846@gmail.com>
2026-01-02 21:59:57 -0300

    feat: create getters and setters

--- src/main/java/com/example/spring_boot_project/model/ChatRoom.java
+++ src/main/java/com/example/spring_boot_project/model/ChatRoom.java
@@ -14,4 +14,36 @@ public class ChatRoom {
     private String clientId;
     private String attendantId;
     private boolean active;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setAttendantId(String attendantId) {
+        this.attendantId = attendantId;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getAttendantId() {
+        return attendantId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
 }
